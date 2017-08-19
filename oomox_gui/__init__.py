02ef42261cd879b1462594271722b6c7e869cd76
actionless <actionless.loveless@gmail.com>
2017-08-19 18:50:39 +0200

    chore: remove files not related to the gtk theme

--- oomox_gui/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import gettext
-
-
-_localedir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                          '..', 'locale')
-gettext.install('oomox', _localedir)
