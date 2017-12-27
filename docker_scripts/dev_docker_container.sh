93bbd368ff71c091f1562d92277974f08a4d82da
actionless <actionless.loveless@gmail.com>
2017-12-27 16:03:07 +0100

    chore(screenshots): update for latest notebook changes

--- docker_scripts/dev_docker_container.sh
+++ docker_scripts/dev_docker_container.sh
@@ -4,5 +4,6 @@ exec docker \
 	container run \
 	--detach --tty \
 	--volume $(readlink -e ./):/opt/oomox-gtk-theme \
+	--volume $(readlink -e ./test_results/):/opt/oomox-gtk-theme/test_results \
 	$@ \
 	oomox-gtk-theme:latest
