3fb2369444145cb61bf2d856bfaba659d2eb478a
actionless <actionless.loveless@gmail.com>
2018-12-28 19:41:30 +0100

    style(maintenance_scripts): shellcheck

--- test/shellcheck.sh
+++ test/shellcheck.sh
@@ -8,4 +8,7 @@ SCRIPT_DIR="$(readlink -e "${TEST_DIR}"/..)"
 
 cd "${SCRIPT_DIR}"
 #hellcheck "$(find "${SCRIPT_DIR}" -name '*.sh')"
-shellcheck -x scripts/*.sh ./change_color.sh
+shellcheck -x ./change_color.sh ./scripts/*.sh ./maintenance_scripts/*.sh
+
+echo '$$ shellcheck passed $$'
+exit 0
