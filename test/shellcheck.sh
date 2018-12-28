d9e3a99fb72a0b80ce78aa1e23e4b4bf059cd859
actionless <actionless.loveless@gmail.com>
2018-12-28 20:53:30 +0100

    style(src): shellcheck

--- test/shellcheck.sh
+++ test/shellcheck.sh
@@ -6,9 +6,10 @@ TEST_DIR=$(readlink -e "$(dirname "${0}")")
 SCRIPT_DIR="$(readlink -e "${TEST_DIR}"/..)"
 
 
-cd "${SCRIPT_DIR}"
 #hellcheck "$(find "${SCRIPT_DIR}" -name '*.sh')"
-shellcheck -x ./change_color.sh ./scripts/*.sh ./maintenance_scripts/*.sh ./test/*.sh
+cd "${SCRIPT_DIR}"
+# shellcheck disable=SC2046
+shellcheck -x ./change_color.sh ./scripts/*.sh ./maintenance_scripts/*.sh ./test/*.sh $(find ./src/ -name '*.sh')
 
 echo '$$ shellcheck passed $$'
 exit 0
