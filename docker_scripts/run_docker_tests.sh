08e0c5b7aef3e3cb169dd0fe6517ebee00b1bd77
actionless <actionless.loveless@gmail.com>
2018-12-28 20:59:44 +0100

    style: shellcheck all .sh files

--- docker_scripts/run_docker_tests.sh
+++ docker_scripts/run_docker_tests.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
 
 # To generate new screenshots:
 # ${0} -e GENERATE_ASSETS=1
 
-cd $(readlink -e $(dirname "${0}"))/..
+cd "$(readlink -e "$(dirname "${0}")")"/..
 container_name=$(docker container ls --quiet --filter ancestor=oomox-gtk-theme:latest)
 if [[ -z ${container_name} ]] ; then
 	echo "Container is not running"
 	exit 1
 fi
 exec docker container exec --interactive --tty \
-	$@ \
-	${container_name} \
+	"$@" \
+	"${container_name}" \
 	./test/all_tests.sh
