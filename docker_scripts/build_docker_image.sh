27a433d111b6949de0333271607d1fa5c31692b2
actionless <actionless.loveless@gmail.com>
2018-12-28 20:59:44 +0100

    style: shellcheck all .sh files

--- docker_scripts/build_docker_image.sh
+++ docker_scripts/build_docker_image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd $(readlink -e $(dirname "${0}"))/..
+cd "$(readlink -e "$(dirname "${0}")")"/.. &&
 exec docker image build ./ -t oomox-gtk-theme
--- docker_scripts/kill_docker_container.sh
+++ docker_scripts/kill_docker_container.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
 
-cd $(readlink -e $(dirname "${0}"))/..
+
+cd "$(readlink -e "$(dirname "${0}")")"/..
 container_name=$(docker container ls --quiet --filter ancestor=oomox-gtk-theme:latest)
 if [[ -z ${container_name} ]] ; then
 	echo "Container is not running"
 	exit 1
 fi
 exec docker container kill \
-	$@ \
-	${container_name}
+	"$@" \
+	"${container_name}"
