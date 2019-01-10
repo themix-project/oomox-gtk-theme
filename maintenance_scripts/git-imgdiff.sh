c9dbd6fcf3ff6564f0d018103229d51b99f3cf0e
actionless <actionless.loveless@gmail.com>
2019-01-10 23:55:10 +0100

    feat(gtk320: button, entry): unify left/right padding and make it depend on roundness to make text readable on the sides in case of too big border radius

--- maintenance_scripts/git-imgdiff.sh
+++ maintenance_scripts/git-imgdiff.sh
@@ -2,9 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-
 temp=$(mktemp --suffix .png)
-compare "$2" "$1" png:- | montage -geometry +4+4 "$2" - "$1" png:- > "${temp}"
+(compare "$2" "$1" png:- || true) | montage -geometry +4+4 "$2" - "$1" png:- > "${temp}"
 #sxiv -N "$1" "${temp}"
 viewnior "${temp}"
 rm "${temp}"
