19201d62833b12dd5586cc9dc5e655c03063e4c4
actionless <actionless.loveless@gmail.com>
2022-10-12 19:13:18 +0200

    wip(test): try increasing similarity threshold once again

--- test/test.sh
+++ test/test.sh
@@ -100,7 +100,7 @@ make_and_compare_screenshot() {
 	compare_result=0
 	compare_output=$(mktemp)
 	compare -verbose -metric AE -fuzz 10 \
-		-similarity-threshold 2 \
+		-similarity-threshold 21 \
 		"${SCREENSHOTS_DIR}/${screenshot_base_name}.png" \
 		"${TEST_RESULT_DIR}/${test_result_base_name}.test.png" \
 		"${TEST_RESULT_DIR}/${test_result_base_name}.diff.png" \
